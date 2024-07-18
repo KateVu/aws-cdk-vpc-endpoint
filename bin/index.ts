cdf5547bd4a2d8fb2ffc2115fc60bfe8944738c8
KateVu <tuquyen87@gmail.com>
2024-07-19 00:16:04 +1000

    change using pipeline

--- bin/index.ts
+++ bin/index.ts
@@ -13,10 +13,13 @@ const accountFileName = 'aws_account.yaml'
 const envName = process.env.ENVIRONMENT_NAME || 'kate'
 const accountName = process.env.ACCOUNT_NAME || 'sandpit1'
 const region = process.env.REGION || 'ap-southeast-2'
+const aws_account_id = process.env.AWS_ACCOUNT_ID || 'none'
 
 //Get aws account id
-const accountId = getAccountId(accountName, configFolder, accountFileName)
-
+let accountId = aws_account_id
+if (aws_account_id == 'none') {
+  accountId = getAccountId(accountName, configFolder, accountFileName)
+}
 const app = new cdk.App();
 const vpcEndpointStack = new AwsCdkVpcEndpointStack(app, 'AwsCdkVpcEndpointStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
@@ -45,16 +48,4 @@ cdk.Tags.of(vpcEndpointStack).add('createdby', 'KateVu')
 cdk.Tags.of(vpcEndpointStack).add('createdvia', 'AWS-CDK')
 cdk.Tags.of(vpcEndpointStack).add('environment', envName)
 cdk.Tags.of(vpcEndpointStack).add('repo', 'https://github.com/KateVu/aws-cdk-vpc-endpoint')
-
-const ec2Instance = new Ec2InstanceStack(app, 'Ec2InstanceTest', {
-  stackName: `vpc-simple-ec2-${envName}`,
-  region: region,
-  accountId: accountId,
-  accountName: accountName,
-  envName: envName,
-})
-
-cdk.Tags.of(ec2Instance).add('createdby', 'KateVu')
-cdk.Tags.of(ec2Instance).add('createdvia', 'AWS-CDK')
-cdk.Tags.of(ec2Instance).add('environment', envName)
-cdk.Tags.of(ec2Instance).add('repo', 'https://github.com/KateVu/aws-cdk-vpc-endpoint')
+cdk.Tags.of(vpcEndpointStack).add('DONOTDELETE', 'true')
